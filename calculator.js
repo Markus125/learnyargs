1103f8621ed97f4eba3d0966c9a800b96939efbc
markus kaljo <markus.kaljo@tptlive.ee>
2023-12-11 10:32:51 +0200

    add calculator

---
+++ calculator.js
@@ -0,0 +1,47 @@
+const yargs = require('yargs');
+
+const argv = yargs
+
+    .option ('sign', {
+    describe: 'Mathematical operation sign',
+    demandOption: true,
+    choices: ['+', '-', '*', '/'],
+})
+
+  .option ('value1', {
+    describe: 'First value',
+    demandOption: true,
+    type: 'number',
+  })
+
+  .option ('value2', {
+    describe: 'Second value',
+    demandOption: true,
+    type: 'number',
+  })
+
+
+  .help()
+  .argv;
+
+
+const calculate = (value1, value2, sign) => {
+  switch (sign) {
+    case '+':
+      return value1 + value2;
+
+    case '-':
+      return value1 - value2;
+
+    case '/':
+      return value1 / value2;
+
+    case '*':
+      return value1 * value2;
+
+    default:
+      return 'Invalid mathematical sign';
+  }
+};
+
+console.log(`${argv.value1} ${argv.sign} ${argv.value2} = ${calculate(argv.value1, argv.value2, argv.sign)}`);
